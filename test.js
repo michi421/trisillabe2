d72934f21c88b75ace0272550d424fa50bf2e85e
spartacus04 <52862005+spartacus04@users.noreply.github.com>
2020-03-20 21:40:43 +0100

    main commit

---
+++ test.js
@@ -0,0 +1,65 @@
+var imagenames = ["banana", "bidone", "binari", "budino", "cabina", "canile", "corona", "numeri", "pagine", "pirata", "posate", "retino", "rumore", "sedici", "tavola", "tunica", "vasino", "vigile", "vipera", "vocali"];
+var lastrng = 0;
+var imagerng;
+var correct;
+
+
+function newimage() {
+    while (true) {
+        imagerng = Math.floor(Math.random() * 100);
+
+        if (imagerng <= 20) {
+            if (imagerng != 0) {
+                if (imagerng != lastrng) {
+                    if (imagerng != undefined) {
+                        lastrng = imagerng;
+                        document.getElementById('image').setAttribute("src", './res/images/' + imagenames[imagerng] + '.png');
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+function check() {
+    if (document.getElementById('text').value.length == 6) {
+        document.getElementById('text').setAttribute("readonly", true);
+        if (document.getElementById('text').value.toLowerCase() == imagenames[imagerng]) {
+            correct = true;
+        } else {
+            correct = false;
+        }
+        setTimeout(hidecontrols, 3000);
+    }
+}
+
+var hidecontrols = function () {
+
+    if (correct) {
+        document.getElementById('emo').setAttribute("src", './res/smiley/smile' + imagerng + '.jpg');
+        console.log("si");
+        newimage();
+
+    } else {
+        document.getElementById('emo').setAttribute("src", './res/ops.JPG');
+        console.log("no")
+    }
+
+
+    document.getElementById('image').style.visibility = "hidden";
+    document.getElementById('text').style.visibility = "hidden";
+    document.getElementById('emo').style.visibility = "visible";
+    document.getElementById('text').value = null;
+
+    setTimeout(showcontrols, 3000);
+}
+
+var showcontrols = function () {
+    document.getElementById('image').style.visibility = "visible";
+    document.getElementById('text').style.visibility = "visible";
+    document.getElementById('emo').style.visibility = "hidden";
+    document.getElementById('text').focus();
+    document.getElementById('text').removeAttribute("readonly")
+
+}
